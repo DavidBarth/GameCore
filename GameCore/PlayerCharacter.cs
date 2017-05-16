99611ebe936ddcf852a4c81a30b6cd291d07416e
David Barth <davidbarth80@gmail.com>
2017-05-16 20:42:16 +0100

    Implement PlayerCHaracter class

---
+++ GameCore/PlayerCharacter.cs
@@ -0,0 +1,65 @@
+﻿namespace GameCore
+{
+    /// <summary>
+    /// Player character class
+    /// </summary>
+    class PlayerCharacter
+    {
+        public int Health { get; private set; } = 100;
+
+        public bool  IsDead { get;private set; }
+
+        /// <summary>
+        /// decrements health by the amount of damage
+        /// </summary>
+        /// <param name="damage"></param>
+        public void HitOnHead(int damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0)
+                IsDead = true;
+        }
+
+        /// <summary>
+        /// decrements health by the amount of damage
+        /// </summary>
+        /// <param name="damage"></param>
+        public void HitOnBody(int damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0)
+                IsDead = true;
+
+        }
+
+        /// <summary>
+        /// decrements health by the amount of damage
+        /// </summary>
+        /// <param name="damage"></param>
+        public void HitOnArm(int damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0)
+                IsDead = true;
+
+        }
+
+        /// <summary>
+        /// decrements health by the amount of damage
+        /// </summary>
+        /// <param name="damage"></param>
+        public void HitOnLeg(int damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0)
+                IsDead = true;
+
+        }
+
+        
+    }
+}
